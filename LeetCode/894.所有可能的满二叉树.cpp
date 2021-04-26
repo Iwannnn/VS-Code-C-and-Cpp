8b7263be972bc5ed6cb7553e848a6fb8dd0cc293
iwannnn <Zhangyaowen11@icloud.com>
2021-04-26 21:38:05 +0800

    2021.4.25

---
+++ LeetCode/894.所有可能的满二叉树.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=894 lang=cpp
+ *
+ * [894] 所有可能的满二叉树
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
+    }
+};
+
+// @lc code=start
+
+class Solution {
+  public:
+    vector<TreeNode *> allPossibleFBT(int n) {
+        vector<TreeNode *> res;
+        return search(n);
+    }
+
+    vector<TreeNode *> search(int n) {
+        // if (n % 2 == 0) return;
+        // if (n == 1) {
+        //     out.left = 0;
+        //     res.push_back(out);
+        // }
+        // for (int i = 1; i < n; i += 2) {
+        //     vector<TreeNode *> left = search(i, res);
+        // }
+        if (n == 1) return {new TreeNode(0)};
+        vector<TreeNode *> res;
+        for (int i = 1; i < n; i += 2) {
+            vector<TreeNode *> left = search(i);
+            vector<TreeNode *> right = search(n - i - 1);
+            if (!left.size() && !right.size()) res.push_back(new TreeNode(0));
+            for (TreeNode *lnode : left) {
+                for (TreeNode *rnode : right) {
+                    TreeNode *root = new TreeNode(0);
+                    root->left = lnode;
+                    root->right = rnode;
+                    res.push_back(root);
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code = end
