8ae91095c0593f832e4ecd8d63dbd4dd4827c68e
iwannnn <Zhangyaowen11@icloud.com>
2021-05-18 19:38:46 +0800

    2021.5.17

---
+++ LeetCode/292.nim-游戏.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=292 lang=cpp
+ *
+ * [292] Nim 游戏
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    bool canWinNim(int n) {
+        return n % 4;
+    }
+};
+// @lc code=end
