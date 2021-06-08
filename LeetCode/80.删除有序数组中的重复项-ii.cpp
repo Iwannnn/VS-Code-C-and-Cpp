ea3007750d53bded336c7cc5a051b2c6e859dbe2
iwannnn <iwannnn1206@163.com>
2021-06-08 21:13:51 +0800

    2021.6.8

---
+++ LeetCode/80.删除有序数组中的重复项-ii.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=80 lang=cpp
+ *
+ * [80] 删除有序数组中的重复项 II
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    int removeDuplicates(vector<int> &nums) {
+        int i = 0;
+        for (int num : nums) {
+            if (i < 2 || num > nums[i - 2]) {
+                nums[i++] = num;
+            }
+        }
+        return i;
+    }
+};
+// @lc code=end
