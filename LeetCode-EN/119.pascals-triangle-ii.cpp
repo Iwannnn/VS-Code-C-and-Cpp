35d9984eb0216bd836509c8acbd699577d155db8
iwannnn <iwannnn1206@163.com>
2021-06-16 23:08:10 +0800

    2021.6.16

---
+++ LeetCode-EN/119.pascals-triangle-ii.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=119 lang=cpp
+ *
+ * [119] Pascal's Triangle II
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res(rowIndex + 1);
+        res[0] = 1;
+        for (int i = 1; i <= rowIndex; ++i) {
+            for (int j = i; j >= 1; --j) {
+                res[j] += res[j - 1];
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
